e324010394bebfef9c69441a3e6b7cf2260e320d
Adriano Fernandes <afernandes130@hotmail.com>
2020-11-25 21:13:35 -0300

    adicionado novos comandos na criacao do container

--- Inicializacao/Run.sh
+++ Inicializacao/Run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+#Configuracao para uso do repositorio na AWS
+# aws configure set aws_access_key_id $AWS_ACCESS_KEY
+# aws configure set aws_secret_access_key $AWS_SECRET_KEY
+# aws configure set default.region $REGION
+# aws s3 sync s3://projeto-pentaho-bi/DockerPDI/Repositorio $PDI_REPOSITORY
+# aws s3 sync s3://projeto-pentaho-bi/DockerPDI/Configuracao $KETTLE_HOME
+
+#Configuração para uso do repositorio no git
 cp -R /tmp/git/Repositorio/* $PDI_REPOSITORY
 cp -R /tmp/git/Configuracao/.kettle/* $KETTLE_HOME
 
 echo ./pan.sh 
 echo ./pan.sh dentro do arquivo
+
+#TODO Execução da tranformação ou JOB passado por pametro
+#$PDI_HOME/data-integration/pan.sh -rep=BiCloud -trans /TesteDocker
