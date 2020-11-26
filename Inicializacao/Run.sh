f015e0e6a351715a6a177cffd077a24fe04d16d8
Adriano Fernandes <afernandes130@hotmail.com>
2020-11-25 21:00:14 -0300

    adicionado novos comandos na criacao do container

--- Inicializacao/Run.sh
+++ Inicializacao/Run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+cp -R /tmp/git/Repositorio/* $PDI_REPOSITORY
+cp -R /tmp/git/Configuracao/.kettle/* $KETTLE_HOME
+
+echo ./pan.sh 
+chmod -R 777 /tmp/git/
+
+
 echo ./pan.sh dentro do arquivo
